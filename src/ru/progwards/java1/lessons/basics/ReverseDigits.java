2c385f79deeed5c0205f8e28113230652ab47929
Skalsoft <juren777@mail.ru>
2020-05-29 16:46:34 +0300

    ReverseDigits numbers only

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -1,16 +1,14 @@
 package ru.progwards.java1.lessons.basics;
 
-import com.sun.security.jgss.GSSUtil;
-
 public class ReverseDigits {
 
     public static int reverseDigits(int number){
-        String rev = Integer.toString(number%10) + (number /= 10)%10 + (number / 10)%10;
-        return Integer.parseInt(rev);
+        return (number%10)*100 + ((number /= 10)%10)*10 + number / 10 %10;
+
     }
 
     public static void main(String[] args) {
-        System.out.println(reverseDigits(123));
+        System.out.println(reverseDigits(538));
 
     }
 }
