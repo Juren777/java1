c7cb7f9afce66abf5acfa229cabd40fe5ca524c6
Skalsoft <juren777@mail.ru>
2020-05-30 17:23:07 +0300

    H5

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -16,4 +16,23 @@ public class ComplexNum {
         return new ComplexNum((a + num.a), (b += num.b));
     //    (a + bi) + (c + di) = (a + c) + (b + d)i
     }
+
+    public ComplexNum sub(ComplexNum num) {
+        return new ComplexNum((a - num.a), (b - num.b));
+//        (a + bi) - (c + di) = (a - c) + (b - d)i
+    }
+
+    public ComplexNum mul(ComplexNum num){
+        return new ComplexNum((a*num.a - b*num.b) , (b*num.a + a*num.b));
+//        (a + bi) * (c + di) = (a*c - b*d) + (b*c + a*d)i
+    }
+
+    public ComplexNum div(ComplexNum num){
+        return new ComplexNum(
+                ((a*num.a + b*num.b )/(a*a +num.b*num.b))
+                ,((b*num.a - a*num.b)/(num.a*num.a + num.b*num.b))
+//        (a + bi) / (c + di) = (a*c + b*d)/(c*c+d*d) + ((b*c - a*d)/(c*c+d*d))i
+        );
+    }
+
 }
