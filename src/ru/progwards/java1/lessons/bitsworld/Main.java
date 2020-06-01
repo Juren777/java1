c0381245fe476b8445e2ba3c639b2f2f09abb6df
Skalsoft <juren777@mail.ru>
2020-06-01 17:50:20 +0300

    H7 - 1

--- src/ru/progwards/java1/lessons/bitsworld/Main.java
+++ src/ru/progwards/java1/lessons/bitsworld/Main.java
@@ -8,10 +8,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-//        System.out.println(" -- sumBits  = " + sumBits((byte) 0b101_1101));
+//        System.out.println(" -- sumBits  = " + sumBits((byte) 0b10000000));
 //        System.out.println(" -- checkBit = " + checkBit((byte) 0b0101_0001,4));
-        Binary binary = new Binary((byte) 17);
-        System.out.println(binary.toString());
+//        Binary binary = new Binary((byte) 17);
+//        System.out.println(binary.toString());
 
 
 
