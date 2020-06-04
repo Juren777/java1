0d34c79327d47c6fcc5469a282fffe7327c944ea
Skalsoft <juren777@mail.ru>
2020-06-04 09:15:16 +0300

    H8 - 1

--- src/ru/progwards/java1/lessons/classes/Animal.java
+++ src/ru/progwards/java1/lessons/classes/Animal.java
@@ -1,10 +1,31 @@
 package ru.progwards.java1.lessons.classes;
 
-public class Animal {
+import ru.progwards.java1.lessons.interfaces.FoodCompare;
+
+import java.util.Objects;
+
+public class Animal implements FoodCompare{
+
+    @Override
+    public int compareFoodPrice(Animal animal) {
+        return Double.compare(this.getFoodPrice(), animal.getFoodPrice());
+    }
+
     enum AnimalKind {ANIMAL, COW, HAMSTER, DUCK}
 
     enum FoodKind {UNKNOWN, HAY, CORN}
 
+    public double getFood1kgPrice(){
+        switch (getFoodKind()){
+            case HAY: return 20;
+            case CORN: return 50;
+            default: return 0;
+        }
+
+    }
+    public double getFoodPrice(){
+        return calculateFoodWeight() * getFood1kgPrice();
+    }
     double weight;
 
     public Animal(double weight) {
@@ -38,6 +59,19 @@ public class Animal {
     public String toStringFull(){
         return "I am " + getKind() + ", eat " + getFoodKind() + " " + calculateFoodWeight();
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Double.compare(animal.weight, weight) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(weight);
     }
 
 }
---
+++ src/ru/progwards/java1/lessons/interfaces/Main.java
@@ -0,0 +1,13 @@
+package ru.progwards.java1.lessons.interfaces;
+
+import ru.progwards.java1.lessons.classes.Animal;
+import ru.progwards.java1.lessons.classes.Duck;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal duck = new Duck(20);
+        Animal duck1 = new Duck(20);
+        System.out.println(duck.equals(duck1));
+        System.out.println(duck.compareFoodPrice(duck1));
+    }
+}
