7d5e8f6513d8c5695174a2320a622940e7faf1af
Skalsoft <juren777@mail.ru>
2020-08-10 17:58:37 +0300

    H11 - 3

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -1,8 +1,37 @@
 package ru.progwards.java1.lessons.io2;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class Censor {
 
     public static void censorFile(String inoutFileName, String[] obscene){
+        try(RandomAccessFile raf = new RandomAccessFile(inoutFileName, "rw")
+        ){
+            String str;
+            StringBuilder out = new StringBuilder();
+            while ((str = raf.readLine()) != null){
+                for (String s: obscene
+                     ) {
+                    int l = s.length();
+                    StringBuilder to = new StringBuilder();
+                    for (int i = 0; i < l; i++){
+                        to.append("*");
+                    }
+                    str = str.replaceAll(s,to.toString());
 
+                }
+                out.append(str + '\n');
+            }
+            raf.seek(0);
+            raf.writeBytes(out.toString().trim());
+            System.out.println(out);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/ru/progwards/java1/lessons/io2/Main.java
+++ src/ru/progwards/java1/lessons/io2/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         //    System.out.println(PhoneNumber.format("8(999)111-22-33"));
-        Translator tr = new Translator(new String[]{"hello", "world"}, new String[]{"привет", "мир"});
-        String out = tr.translate("Hello, World!!!");
-        System.out.println(out);
+//        Translator tr = new Translator(new String[]{"hello", "world"}, new String[]{"привет", "мир"});
+//        String out = tr.translate("Hello, World!!!");
+//        System.out.println(out);
+        Censor.censorFile("file.txt", new String[]{"storey", "counts", "two", "day", "count", "write"});
     }
 }
