65d7f79771f7362e5f6713103429cd28af9aaea5
Skalsoft <juren777@mail.ru>
2020-08-13 19:26:42 +0300

    H12 - 1

---
+++ src/ru/progwards/java1/lessons/collections/Creator.java
@@ -0,0 +1,36 @@
+package ru.progwards.java1.lessons.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Creator {
+
+    public static Collection<Integer> fillEven(int n) {
+        Collection<Integer> ci = new ArrayList<>();
+        for (int i = 2; i < n * 2 + 2; i += 2) {
+            ci.add(i);
+        }
+        return ci;
+    }
+
+    public static Collection<Integer> fillOdd(int n) {
+        Collection<Integer> ci = new ArrayList<>();
+        for (int i = 1; i < n * 2; i += 2) {
+            ci.add(i);
+        }
+        Collections.reverse((List<?>) ci);
+        return ci;
+    }
+
+    public static Collection<Integer> fill3(int n){
+        Collection<Integer> ci = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ci.add(i);
+            ci.add(i*i);
+            ci.add(i*i*i);
+        }
+        return ci;
+    }
+}
---
+++ src/ru/progwards/java1/lessons/collections/Main.java
@@ -0,0 +1,11 @@
+package ru.progwards.java1.lessons.collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Creator.fillEven(6));
+        System.out.println(Creator.fillOdd(6));
+        System.out.println(Creator.fill3(4));
+    }
+}
