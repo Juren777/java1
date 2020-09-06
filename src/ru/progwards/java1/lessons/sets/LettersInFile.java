3ce5b558d913a009eaf999737ab81ad20976cad5
Skalsoft <juren777@mail.ru>
2020-09-06 16:24:57 +0300

    H13 - 2

---
+++ src/ru/progwards/java1/lessons/sets/LettersInFile.java
@@ -0,0 +1,42 @@
+package ru.progwards.java1.lessons.sets;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class LettersInFile {
+
+    public static String process(String fileName){
+        TreeSet<Character> treeSet = new TreeSet<>(new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        SortedSet<Character> subset = null;
+        try(FileReader fileReader = new FileReader(fileName);
+            Scanner scanner = new Scanner(fileReader)
+        ){
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                for (char ch : line.toCharArray()) {
+
+                    if (ch >= 65 && ch <= 90 || ch >= 97 && ch <= 122 || ch >= 1040 && ch <= 1103)
+                        treeSet.add(ch);
+                }
+            }
+            subset = treeSet.subSet('A', 'я');
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return subset.toString();
+    }
+
+    public static void main(String[] args) {
+        process("file.txt");
+    }
+}
