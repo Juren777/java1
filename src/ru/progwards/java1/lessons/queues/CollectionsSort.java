d0ebcfaa688f3cc1ead9299b134f2ef514d7b5ff
Skalsoft <juren777@mail.ru>
2020-09-22 21:26:55 +0300

    H14 - 4

--- src/ru/progwards/java1/lessons/queues/CollectionsSort.java
+++ src/ru/progwards/java1/lessons/queues/CollectionsSort.java
@@ -3,20 +3,37 @@ package ru.progwards.java1.lessons.queues;
 import java.util.*;
 
 public class CollectionsSort {
+
+//    public static void mySort(Collection<Integer> data) {
+//        Integer[] a = data.toArray(new Integer[data.size()]);
+//        for (int i = 0; i < a.length; i++) {
+//            for (int j = i + 1; j < a.length; j++) {
+//                int c;
+//                if (a[i] > a[j]) {
+//                    c = a[i];
+//                    a[i] = a[j];
+//                    a[j] = c;
+//                }
+//            }
+//        }
+//        data.clear();
+//        data.addAll(Arrays.asList(a));
+//    }
+
     public static void mySort(Collection<Integer> data) {
-        Integer[] a = data.toArray(new Integer[data.size()]);
-        for (int i = 0; i < a.length; i++) {
-            for (int j = i + 1; j < a.length; j++) {
+        List<Integer> a = new ArrayList<>();
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = i + 1; j < a.size(); j++) {
                 int c;
-                if (a[i] > a[j]) {
-                    c = a[i];
-                    a[i] = a[j];
-                    a[j] = c;
+                if (a.get(i) > a.get(j)) {
+                    c = a.get(i);
+                    a.set(i, a.get(j));
+                    a.set(j, c);
                 }
             }
         }
         data.clear();
-        data.addAll(Arrays.asList(a));
+        data.addAll(a);
     }
     public static void minSort(Collection<Integer> data){
         Collection<Integer> newData = new ArrayList<>();
