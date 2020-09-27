57133527fa756d7e4de9a96b7c18163d89de8185
Skalsoft <juren777@mail.ru>
2020-09-27 16:01:18 +0300

    H15 - 2

--- src/ru/progwards/java1/lessons/maps/UsageFrequency.java
+++ src/ru/progwards/java1/lessons/maps/UsageFrequency.java
@@ -24,7 +24,7 @@ public class UsageFrequency {
     public Map<Character, Integer> getLetters(){
         Map<Character, Integer> map = new TreeMap<>();
 
-        try(FileReader fr = new FileReader("wiki.train.tokens")){
+        try(FileReader fr = new FileReader("wiki.test.tokens")){
             int data;
             char ch;
             while ((data = fr.read()) != -1){
@@ -51,7 +51,7 @@ public class UsageFrequency {
         char ch;
         StringBuilder sb = new StringBuilder();
         String str;
-        try(FileReader fr = new FileReader("wiki.train.tokens")){
+        try(FileReader fr = new FileReader("wiki.test.tokens")){
             while ((data = fr.read()) != -1){
                 if (Character.isLetter(data)||Character.isDigit(data)){
                     ch = (char) data;
