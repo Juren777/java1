305e81624987eca914f48bb445bfdf141f12ef5d
Skalsoft <juren777@mail.ru>
2020-10-11 12:32:21 +0300

    H16 - 2

--- src/ru/progwards/java1/lessons/datetime/Insurance.java
+++ src/ru/progwards/java1/lessons/datetime/Insurance.java
@@ -2,6 +2,8 @@ package ru.progwards.java1.lessons.datetime;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAccessor;
 
 public class Insurance {
 
@@ -11,16 +13,13 @@ public class Insurance {
 
     private Duration duration; // продолжительность действия.
 
-    public ZonedDateTime getStart() {
-        return start;
-    }
-
-    public void setStart(ZonedDateTime start) {
+    // установить дату-время начала действия страховки.
+    public Insurance(ZonedDateTime start){
         this.start = start;
     }
 
-    public Duration getDuration() {
-        return duration;
+    public ZonedDateTime getStart() {
+        return start;
     }
 
     //  установить продолжительность действия страховки.
@@ -33,10 +32,35 @@ public class Insurance {
         this.duration =  Duration.between(getStart(),expiration);
 
     }
+    // установить продолжительность действия страховки, задав целыми числами количество месяцев, дней и часов.
+    public void setDuration(int months, int days, int hours){
+        ZonedDateTime endTime = start.plusMonths(months).plusDays(days).plusHours(hours);
+        this.duration =  Duration.between(getStart(), endTime);
+    }
 
-    // установить дату-время начала действия страховки.
-    public Insurance(ZonedDateTime start){
-        this.start = start;
+    // установить продолжительность действия страховки
+    public void setDuration(String strDuration, FormatStyle style){
+        if (style.equals(FormatStyle.SHORT)){
+            this.duration = Duration.ofMillis(Long.parseLong(strDuration));
+        }else if (style.equals(FormatStyle.LONG)){
+            DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+            TemporalAccessor ta = dtf.parse(strDuration);
+
+            ZonedDateTime endTime = start.plusYears(ta.get(ChronoField.YEAR))
+                    .plusMonths(ta.get(ChronoField.MONTH_OF_YEAR))
+                    .plusDays(ta.get(ChronoField.DAY_OF_MONTH))
+                    .plusHours(ta.get(ChronoField.HOUR_OF_DAY))
+                    .plusMinutes(ta.get(ChronoField.MINUTE_OF_HOUR))
+                    .plusSeconds(ta.get(ChronoField.SECOND_OF_MINUTE));
+
+            this.duration = Duration.between(start, endTime);
+        } else if (style.equals(FormatStyle.FULL)){
+            this.duration = Duration.parse(strDuration);
+        }
+    }
+
+    public Duration getDuration() {
+        return duration;
     }
 
     // проверить действительна ли страховка на указанную дату-время. Если продолжительность
@@ -52,15 +76,15 @@ public class Insurance {
     // установить дату-время начала действия страховки.
     public Insurance(String strStart, FormatStyle style){
 
-        if (style == FormatStyle.SHORT){
+        if (style.equals(FormatStyle.SHORT)){
             DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_DATE;
             LocalDate ld = LocalDate.from(dtf.parse(strStart));
             this.start = ld.atStartOfDay(ZoneId.systemDefault());
-        } else if (style == FormatStyle.LONG){
+        } else if (style.equals(FormatStyle.LONG)){
             DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
             LocalDateTime ld = LocalDateTime.from(dtf.parse(strStart));
             this.start = ld.atZone(ZoneId.systemDefault());
-        } else if (style == FormatStyle.FULL){
+        } else if (style.equals(FormatStyle.FULL)){
             DateTimeFormatter dtf = DateTimeFormatter.ISO_ZONED_DATE_TIME;
             ZonedDateTime zdt = ZonedDateTime.parse(strStart, dtf);
             this.start = zdt;
