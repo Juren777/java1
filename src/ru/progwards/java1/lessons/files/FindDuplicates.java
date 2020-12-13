a1d51bd7e1d63d642503f20ba6be18d16409d5e7
Skalsoft <juren777@mail.ru>
2020-12-13 22:49:19 +0300

    H17 - 0

---
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -0,0 +1,16 @@
+package ru.progwards.java1.lessons.files;
+
+import java.util.List;
+
+public class FindDuplicates {
+// В заданном каталоге и его подкаталогах найти файлы,
+    // точно совпадающие по названию (и расширению), дате-времени последнего изменения, размеру и по содержимому.
+    // результат - список, содержащий списки строк с именами и полными путями совпадающих файлов
+    public List<List<String>> findDuplicates(String startPath){
+
+
+
+        return null;
+    }
+
+}
